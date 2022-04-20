eb2cf40ee5f5d889854958fe08716ac88984f75f
BO133 <bohdanperch@gmail.com>
2022-04-21 01:18:42 +0300

    season.rb update

--- argv/season.rb
+++ argv/season.rb
@@ -3,21 +3,18 @@ season = ARGV[1]
 
 sum_temp = (15..35).to_a
 
-if temp == nil && season != nil
+if temp.nil?
   puts "Введите температуру"
   temp = STDIN.gets.chomp
-elsif season == nil && temp != nil
-  puts "Выберите время года: 0 - весна, 1 - лето, 2 - осень, 3 - зима."
-  season = STDIN.gets.chomp
-elsif season == nil && temp == nil
-  puts "Введите температуру"
-  temp = STDIN.gets.chomp
+end 
+
+if season.nil?
   puts "Выберите время года: 0 - весна, 1 - лето, 2 - осень, 3 - зима."
   season = STDIN.gets.chomp
 end
 
-puts temp.to_s
-puts season.to_s
+#puts temp.to_s
+#puts season.to_s
 #  >= 15 || temp.to_i <= 35
 
 if season.to_i == 1 && temp.to_i >= 15 && temp.to_i <= 35
