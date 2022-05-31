cb0206fe4562dc9367019d348e77579ad8e69421
BO133 <bohdanperch@gmail.com>
2022-05-31 22:36:39 +0300

    Fix movie.rb, letele elsif for 0

--- files/movie.rb
+++ files/movie.rb
@@ -9,13 +9,4 @@ if a.odd?
 elsif a.even?
   puts movie_all[a]
   puts movie_all[a + 1]
-elsif a == 0
-  puts movie_all[a]
-  puts movie_all[a + 1]
 end
-
-# a = [movie_all[0], movie_all[1]]
-# b = [movie_all[2], movie_all[3]]
-# c = [movie_all[4], movie_all[5]]
-# d = [movie_all[6], movie_all[7]]
-# puts [a, b, c, d].sample
